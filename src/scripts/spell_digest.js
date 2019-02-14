b96e3429726707e4323bbd17f32798c29a7a4d90
Joshua Bassin <jbassin@bu.edu>
2019-02-14 15:02:18 -0500

    added format for duration and time

--- src/scripts/spell_digest.js
+++ src/scripts/spell_digest.js
@@ -31,11 +31,30 @@ const componentFormat = component => ({
 });
 const safeComponentFormat = component => componentFormat(safeObject(component));
 
-const spellFormat = spell => {
+const singleDurationFormat = (duration) => {
+    const safeDuration = safeObject(duration.duration);
+    return {
+        type: safeBlank(duration.type).toLowerCase(),
+        time: safeZero(safeDuration.amount),
+        unit: safeBlank(safeDuration.type),
+    };
+};
+const safeSingleDurationFormat = duration => singleDurationFormat(safeObject(duration));
+const durationFormat = spell => R.map(safeSingleDurationFormat, spell.duration);
+
+const timeFormat = time => ({
+    time: safeZero(time.number),
+    unit: safeBlank(time.unit),
+});
+const safeTimeFormat = time => timeFormat(safeObject(time));
+
+const spellFormat = (spell) => {
     name: safeBlank(spell.name),
     source: safeBlank(spell.source),
     level: safeZeroInt(spell.level),
     range: rangeFormat(spell),
     school: safeSchoolFormat(spell.school),
     components: safeComponentFormat(spell.components),
+    durations: durationFormat(spell),
+    time: safeTimeFormat(spell.time),
 };
