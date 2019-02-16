af3e043280cd11dfe40044fc5b9fb2f85accb3c1
Joshua Bassin <jbassin@bu.edu>
2019-02-16 17:58:48 -0500

    added store mutator functions

--- src/main.js
+++ src/main.js
@@ -6,6 +6,28 @@ import store from './store';
 Vue.config.productionTip = false;
 const DEBUG = false;
 
+const R = require('ramda');
+
+Vue.prototype.$R = R;
+
+Vue.prototype.$storeGet = R.curry(function storeGet(namespace, variable) {
+  return this.$store.state[namespace][variable];
+});
+
+Vue.prototype.$storeSet = R.curry(function storeSet(namespace, command, ...rest) {
+  return this.$store.dispatch({
+    type: `${namespace}/${command}`,
+    ...rest,
+  });
+});
+
+Vue.prototype.$storeMutators = function storeMutators(namespace) {
+  return {
+    getter: this.$storeGet(namespace),
+    setter: this.$storeSet(namespace),
+  };
+};
+
 const load = require('./scripts/init')(DEBUG);
 
 console.log(load);
