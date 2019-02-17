93b7b51606f55c0b8d7263127cb9fe3bb2ffd402
Joshua Bassin <jbassin@bu.edu>
2019-02-16 22:51:50 -0500

    created header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <div id="app" class="container">
-    <h1 class="title is-1">
-      Hello
-    </h1>
+  <div id="app">
+    <tb-header/>
   </div>
 </template>
 
 <script>
+import TbHeader from './components/header/tbHeader.vue';
+
 const DEBUG = false;
 const load = require('./scripts/init')(DEBUG);
 
 export default {
   name: 'app',
+  components: { TbHeader },
   created() {
     const { getter: itemGetter, setter: itemSetter } = this.$storeMutators(this, 'items');
     const { getter: spellGetter, setter: spellSetter } = this.$storeMutators(this, 'spells');
@@ -25,6 +26,13 @@ export default {
 </script>
 
 <style>
-  @import "~bulmaswatch/lux/bulmaswatch.min.css";
   @import "~@fortawesome/fontawesome-free/css/all.min.css";
+  @import "~bulmaswatch/lux/bulmaswatch.min.css";
+  @font-face {
+    font-family: 'Nunito Sans';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Nunito Sans Regular'), local('NunitoSans-Regular'), url(https://fonts.gstatic.com/s/nunitosans/v3/pe0qMImSLYBIv1o4X1M8cce9I9s.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+  }
 </style>
---
+++ src/components/header/tbHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container has-text-centered">
+    <div class="notification is-primary">
+      <p class="title is-1">
+        Tabula Rasa
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tbHeader',
+};
+</script>
+
+<style scoped>
+  .container {
+    margin-top: 15px;
+  }
+</style>
