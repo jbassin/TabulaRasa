0b495c4807df4da4457386ab24dd435f8e1fe15b
Joshua Bassin <jbassin@bu.edu>
2019-02-18 00:45:03 -0500

    Added a selector for the homebrew section

---
+++ src/components/homebrew/tbHomebrewSelector.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="tile notification enter-smooth"
+       :class="isSelected">
+    <div class="has-text-centered has-text-white">
+      <p class="is-subtitle is-4">{{ this.name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tbHomebrewSelector',
+  props: {
+    activeSelector: {
+      required: true,
+      type: String,
+    },
+    name: {
+      required: true,
+      type: String,
+    },
+  },
+  computed: {
+    isSelected() {
+      if (this.activeSelector.toLowerCase() === this.name.toLowerCase()) return 'selected';
+      return 'deselected';
+    },
+  },
+};
+</script>
+
+<style scoped>
+  @keyframes select {
+    0% {
+      background-color: #7D858D;
+    }
+    100% {
+      background-color: #318AF1;
+    }
+  }
+  @keyframes deselect {
+    100% {
+      background-color: #7D858D;
+    }
+    0% {
+      background-color: #318AF1;
+    }
+  }
+  .enter-smooth {
+    animation-duration: 1.0s;
+    animation-direction: normal;
+    animation-timing-function: ease-in-out;
+    animation-delay: 2.0s;
+    animation-fill-mode: both;
+    animation-name: fadeIn;
+  }
+  .selected {
+    -webkit-animation: select 1.0s ease-in-out normal both;
+  }
+  .deselected {
+    -webkit-animation: deselect 1.0s ease-in-out normal both;
+  }
+</style>
--- src/views/tbHomebrew.vue
+++ src/views/tbHomebrew.vue
@@ -4,18 +4,54 @@
       <p class="title is-3">
         View and Modify Data:
       </p>
+      <div class="tile is-ancestor">
+        <div class="tile is-4 is-vertical">
+          <tb-homebrew-selector v-for="(name, index) of this.homebrewItems"
+                                :key="index"
+                                :name="name"
+                                :active-selector="activeSelector"
+                                @click.native="activeSelector = name"/>
+        </div>
+        <div class="tile is-right-column notification is-danger">
+          TESTING
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import TbHomebrewSelector from '../components/homebrew/tbHomebrewSelector.vue';
+
 export default {
   name: 'tbHomebrew',
+  components: { TbHomebrewSelector },
+  data() {
+    return {
+      activeSelector: 'Test',
+      homebrewItems: [
+        'Races',
+        'Classes',
+        'Subclasses',
+        'Spells',
+        'Feats',
+        'Items',
+      ],
+    };
+  },
 };
 </script>
 
 <style scoped>
   .container {
     margin-top: 15px;
+  }
+  .is-ancestor {
+    margin-left: 10px;
+    margin-top: 15px;
+    padding-bottom: 15px;
+  }
+  .is-right-column {
+    margin-left: 10px;
   }
 </style>
